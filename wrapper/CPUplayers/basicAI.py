d1bdf8dc2b5cd2d8b68a01ccb32218dc995487e8
Alec Andrews <alecandrews33@gmail.com>
2020-08-08 15:46:02 -0700

    Move player data

--- wrapper/CPUplayers/basicAI.py
+++ wrapper/CPUplayers/basicAI.py
@@ -2,6 +2,11 @@ import sys
 import sdl2
 import sdl2.ext
 
+class PlayerData(object):
+    def __init__(self):
+        super(PlayerData, self).__init__()
+        self.ai = False
+        self.points = 0
 
 class TrackingAIController(sdl2.ext.Applicator):
     def __init__(self, miny, maxy):
