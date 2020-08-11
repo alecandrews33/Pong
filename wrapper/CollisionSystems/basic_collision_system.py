97696654da54a27db104b23dbfc5547783cf8a93
Alec Andrews <alecandrews33@gmail.com>
2020-08-10 21:42:21 -0700

    Reset ball to middle, sleep for 1 second on score.

--- wrapper/CollisionSystems/basic_collision_system.py
+++ wrapper/CollisionSystems/basic_collision_system.py
@@ -19,6 +19,9 @@ class CollisionSystem(sdl2.ext.Applicator):
         self.maxy = maxy
         self.p1data = p1data
         self.p2data = p2data
+        self.score = False
+        self.scoredOnLeftSide = False
+
 
     def _overlap(self, item):
         sprite = item[1]
@@ -31,8 +34,10 @@ class CollisionSystem(sdl2.ext.Applicator):
         return (bleft < right and bright > left and
                 btop < bottom and bbottom > top)
 
-    def ball_score(direction):
-        if (sign(direction) < 1):
+    def ball_score(self, scoredOnLeftSide):
+        self.score = True
+        self.scoredOnLeftSide = scoredOnLeftSide
+        if (scoredOnLeftSide):
             self.p2data.points += 1
         else:
             self.p1data.points += 1
@@ -40,36 +45,40 @@ class CollisionSystem(sdl2.ext.Applicator):
         self.ball.sprite.position = 390, 290
         self.ball.velocity.vx = 0
         self.ball.velocity.vy = 0
-        time.sleep(2)
-        self.ball.velocity.vx = sign(direction) * 3
-        self.ball.velocity.vy = 0
 
     def process(self, world, componentsets):
-        collitems = [comp for comp in componentsets if self._overlap(comp)]
-        if len(collitems) != 0:
-            self.ball.velocity.vx = -self.ball.velocity.vx
+        if (not self.score):
+            collitems = [comp for comp in componentsets if self._overlap(comp)]
+            if len(collitems) != 0:
+                self.ball.velocity.vx = -self.ball.velocity.vx
+                sprite = collitems[0][1]
+                ballcentery = self.ball.sprite.y + self.ball.sprite.size[1] // 2
+                halfheight = sprite.size[1] // 2
+                stepsize = halfheight // 10
+                degrees = 0.7
+                paddlecentery = sprite.y + halfheight
+                if ballcentery < paddlecentery:
+                    factor = (paddlecentery - ballcentery) // stepsize
+                    self.ball.velocity.vy = -int(round(factor * degrees))
+                elif ballcentery > paddlecentery:
+                    factor = (ballcentery - paddlecentery) // stepsize
+                    self.ball.velocity.vy = int(round(factor * degrees))
+                else:
+                    self.ball.velocity.vy = -self.ball.velocity.vy
 
-            sprite = collitems[0][1]
-            ballcentery = self.ball.sprite.y + self.ball.sprite.size[1] // 2
-            halfheight = sprite.size[1] // 2
-            stepsize = halfheight // 10
-            degrees = 0.7
-            paddlecentery = sprite.y + halfheight
-            if ballcentery < paddlecentery:
-                factor = (paddlecentery - ballcentery) // stepsize
-                self.ball.velocity.vy = -int(round(factor * degrees))
-            elif ballcentery > paddlecentery:
-                factor = (ballcentery - paddlecentery) // stepsize
-                self.ball.velocity.vy = int(round(factor * degrees))
-            else:
+            if (self.ball.sprite.y <= self.miny or
+                self.ball.sprite.y + self.ball.sprite.size[1] >= self.maxy):
                 self.ball.velocity.vy = -self.ball.velocity.vy
 
-        if (self.ball.sprite.y <= self.miny or
-            self.ball.sprite.y + self.ball.sprite.size[1] >= self.maxy):
-            self.ball.velocity.vy = -self.ball.velocity.vy
 
-
-        if self.ball.sprite.x <= self.minx:
-            ball_score(-1)
-        elif self.ball.sprite.x + self.ball.sprite.size[0] >= self.maxx:
-            ball_score(1)
+            if self.ball.sprite.x <= self.minx:
+                self.ball_score(True)
+            elif self.ball.sprite.x + self.ball.sprite.size[0] >= self.maxx:
+                self.ball_score(False)
+        else:
+            self.score = False
+            time.sleep(1)
+            if (self.scoredOnLeftSide):
+                self.ball.velocity.vx = -3
+            else:
+                self.ball.velocity.vx = 3
